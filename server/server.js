2b0cf0e69e5ac3183ab037cf1215d1e9f2bcb2ef
madhushanka Deshan <madhushankadeshan96@gmail.com>
2018-04-03 23:33:57 +0530

    Add test cases for GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
-
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -11,10 +10,11 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res) => {
+app.post('/todos', (req, res) => {
   var todo = new Todo({
-    text:req.body.text
+    text: req.body.text
   });
+
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
@@ -26,26 +26,26 @@ app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
-          res.status(400).send(e);
+    res.status(400).send(e);
   });
 });
 
-
-app.get('/todos/:id', (req,res) => {
+app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid(id)) {
-
-    return res.status(404).send();
-
-  }
-Todo.findById(id).then((todo) => {
-  if(!todo){
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-  res.send(todo);
-});
 
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(3000, () => {
