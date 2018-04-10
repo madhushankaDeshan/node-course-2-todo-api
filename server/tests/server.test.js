c80053bd9bfb3151f939998fae9d769a74a73dfb
madhushanka Deshan <madhushankadeshan96@gmail.com>
2018-04-10 16:50:21 +0530

    Update config to use json file

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -174,6 +174,7 @@ describe('PATCH /todos/:id', () => {
 
     request(app)
       .patch(`/todos/${hexId}`)
+      .set('x-auth' , users[0].tokens[0].token)
       .send({
         completed: true,
         text
@@ -187,9 +188,20 @@ describe('PATCH /todos/:id', () => {
       .end(done);
   });
 
+  it('should not update the todo created by other user', (done) => {
+    var hexId = todos[0]._id.toHexString();
+    var text = 'This should be the new text';
 
-
-
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .set('x-auth' , users[1].tokens[0].token)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(404)
+      .end(done);
+  });
 
   it('should clear completedAt when todo is not completed', (done) => {
     var hexId = todos[1]._id.toHexString();
@@ -197,6 +209,7 @@ describe('PATCH /todos/:id', () => {
 
     request(app)
       .patch(`/todos/${hexId}`)
+      .set('x-auth' , users[1].tokens[0].token)
       .send({
         completed: false,
         text
